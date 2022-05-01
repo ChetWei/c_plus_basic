5f238b294e5b49fec86f1979b2d9fcbe493cf22f
wei <chetwei@163.com>
2022-05-01 16:03:09 +0800

    cin输入

--- 05循环和关系表达式/05循环和文本输入.cpp
+++ 05循环和关系表达式/05循环和文本输入.cpp
@@ -2,15 +2,17 @@
 
 using namespace std;
 
+//cin 读取char值，与读取其他基本类型一样，cin将忽略空格和换行符
+
 void test() {
 
     char ch;
-    int count = 0;
+    int count = 0; //统计输入的次数
     cout << "输入字符，#结束: " << endl;
 
     cin >> ch;
     while (ch != '#') {
-        cout << ch;
+        cout << ch; //打印
         ++count;
         cin >> ch; //cin 会忽略 空格 和 换行符
     }
